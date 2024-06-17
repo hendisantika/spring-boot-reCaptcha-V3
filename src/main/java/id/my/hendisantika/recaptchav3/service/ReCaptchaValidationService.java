cc4f2b6c9e581723cbc2a5ac76bbb65f4ebc3b0b
hendisantika <hendisantika@yahoo.co.id>
2024-06-17 13:14:51 +0700

    Add validateCaptcha in ReCaptchaValidationService 🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/recaptchav3/service/ReCaptchaValidationService.java
+++ src/main/java/id/my/hendisantika/recaptchav3/service/ReCaptchaValidationService.java
@@ -1,6 +1,10 @@
 package id.my.hendisantika.recaptchav3.service;
 
+import id.my.hendisantika.recaptchav3.dto.ReCaptchResponseType;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +22,18 @@ public class ReCaptchaValidationService {
     private static final String GOOGLE_RECAPTCHA_ENDPOINT = "https://www.google.com/recaptcha/api/siteverify";
     private final String RECAPTCHA_SECRET = "Your Google reCaptcha secret key";
 
+    public boolean validateCaptcha(String captchaResponse) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        MultiValueMap<String, String> requestMap = new LinkedMultiValueMap<>();
+        requestMap.add("secret", RECAPTCHA_SECRET);
+        requestMap.add("response", captchaResponse);
+
+        ReCaptchResponseType apiResponse = restTemplate.postForObject(GOOGLE_RECAPTCHA_ENDPOINT, requestMap, ReCaptchResponseType.class);
+        if (apiResponse == null) {
+            return false;
+        }
+
+        return Boolean.TRUE.equals(apiResponse.isSuccess());
+    }
 }
