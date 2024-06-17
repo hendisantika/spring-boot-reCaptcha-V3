8ab9006e252808e7be33f4115cbedcacb32960d4
hendisantika <hendisantika@yahoo.co.id>
2024-06-17 13:10:18 +0700

    Create Spring Boot Project 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/recaptchav3/SpringBootReCaptchaV3Application.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.recaptchav3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReCaptchaV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReCaptchaV3Application.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/recaptchav3/SpringBootReCaptchaV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.recaptchav3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReCaptchaV3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
